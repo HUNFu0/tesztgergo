2615f4c8e02b07cd9a3c2c070d83e8dfa772a9cd
Kiss Gergo <kissgergo99924@gmail.com>
2022-01-20 08:02:41 +0100

    haz megvan + első küldi

--- move.py
+++ move.py
@@ -1,1 +1,12 @@
-print("SIU")
+udvar = True
+eloszoba = False
+nappali = False
+szuloszoba = False
+gyerekszoba = False
+konyha = False
+Qegyelesaludjal = False
+
+print("Megérkeztél egy hosszú nap után haza és meg álsz az udvaron.")
+
+for i in range(0,9999,1):
+    print("Hova akarsz menni?")
